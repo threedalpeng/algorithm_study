f0e6f92b2c3ea82072955d71cbe2990bc6937c4a
Kim Jeong-Won <threedalpeng@gmail.com>
2019-09-18 21:16:18 +0900

    Update Week1

--- Week1 Sort Greedy_Algorithm/수 정렬하기 2(2751).cpp
+++ Week1 Sort Greedy_Algorithm/수 정렬하기 2(2751).cpp
@@ -1,40 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-
-int partition(int* arr, int first, int last);
-void quicksort(int* arr, int first, int last);
 
 int main()
 {
     int N;
     cin >> N;
-    
-    int* arr = new int[N];
-    for (int i{0}; i < N; i++)
+    vector<int> v;
+    int input;
+    for(int i = 0; i < N; i++)
     {
-        cin >> arr[i];
+        cin >> input;
+        v.push_back(input);
     }
-    
-
-}
-
-int partition(int* arr, int first, int last)
-{
-    int pivot = arr[first];
-    int left = first + 1;
-    int right = last;
-    while(left < right)
+    sort(v.begin(), v.end());
+    for(auto i : v)
     {
-        while(arr[left] < pivot)
-            left++;
-        while(arr[right] > pivot)
-            right++;
-        if (left <= right)
-        {
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-        }
+        cout << i << '\n';
     }
-    
+    return 0;
 }
