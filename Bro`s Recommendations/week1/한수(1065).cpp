4cbe77ab54d120db1cc1840af753b9e2c7c37eec
Kim Jeong-Won <threedalpeng@gmail.com>
2019-09-25 23:53:01 +0900

    Update Bro's Week1

--- Bro`s Recommendations/week1/한수(1065).cpp
+++ Bro`s Recommendations/week1/한수(1065).cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int N;
     cin >> N;
+    int count = (N >= 100 ? 99 : N);
+    for (int i = 100; i <= N; i++)
+    {
+        int n100 = i / 100;
+        int n10 = (i % 100) / 10;
+        int n1 = i % 10;
+        if (n100 - n10 == n10 - n1) ++count;
+    }
+    cout << count;
+    return 0;
 }
