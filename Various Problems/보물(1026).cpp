629da6160eea8ffba162c8813108d806fb0d600f
Kim Jeong-Won <threedalpeng@gmail.com>
2019-09-25 23:53:26 +0900

    Update Various Problem
    보물(1026)

---
+++ Various Problems/보물(1026).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+    int* A = new int[N];
+    int* B = new int[N];
+    for (int i = 0; i < N; i++) cin>>A[i];
+    for (int i = 0; i < N; i++) cin>>B[i];
+    sort(A, A+N);
+    sort(B, B+N, greater<int>());
+    int sum = 0;
+    for (int i = 0; i < N; i++) sum += A[i]*B[i];
+    cout << sum;
+}
