25a3f260e142f4f83d72942d79ba78f59133366d
Kim Jeong-Won <threedalpeng@gmail.com>
2019-10-05 02:46:31 +0900

    Update Week3

--- Week3_Dynamic_Programming/가장 긴 증가하는 부분 수열(11053).cpp
+++ Week3_Dynamic_Programming/가장 긴 증가하는 부분 수열(11053).cpp
@@ -12,25 +12,17 @@ int main()
     cin >> N;
     for(int i = 0; i < N; i++) cin >> A[i];
     L[0] = 1;
+    int max = 1;
     for (int i = 1; i < N; i++)
     {
-        L[i] = 1;
-        for (int j = i-1; j >= 0; j--)
+        int m = 0;
+        for(int j = 0; j < i; j++)
         {
-            if (A[i] > A[j])
-            {
-                L[i] = L[j] + 1;
-                break;
-            }
-            else if (A[i] == A[j])
-            {
-                L[i] = L[j];
-                break;
-            }
+            if (A[i]>A[j] && m < L[j])
+                m = L[j];
         }
+        L[i] = m + 1;
+        if (max < L[i]) max = L[i];
     }
-    
-    cout << L[N-1];
-    // 20 10 30 0 40
-    // 1  1  2  1  3
+    cout << max;
 }
