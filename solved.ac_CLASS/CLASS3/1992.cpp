e3c8d6abb78dcc87b073d4b20f1de8beec25b44a
Kim Jeong-Won <threedalpeng@gmail.com>
2020-03-10 04:18:48 +0900

    Update solved.ac Class3

--- solved.ac_CLASS/CLASS3/1992.cpp
+++ solved.ac_CLASS/CLASS3/1992.cpp
@@ -6,6 +6,20 @@ using namespace std;
 int N;
 vector<string> vs;
 
+string divide(int x, int y, int n)
+{
+    if (n == 1) return vs[x].substr(y, 1);
+    n /= 2;
+    string s1 = divide(x, y, n);
+    string s2 = divide(x, y + n, n);
+    string s3 = divide(x + n, y, n);
+    string s4 = divide(x + n, y + n, n);
+    if (s1.length() == 1 && s1 == s2 && s2 == s3 && s3 == s4)
+        return s1;
+    else
+        return "("s + s1 + s2 + s3 + s4 + ")"s;
+}
+
 int main()
 {
     cin >> N;
@@ -15,9 +29,5 @@ int main()
         cin >> si;
         vs.push_back(si);
     }
-    int x = 0, y = 0, n=1;
-    while(true)
-    {
-        
-    }
+    cout << divide(0, 0, N);
 }
