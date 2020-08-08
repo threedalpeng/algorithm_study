69524a5b38e9e157c190f3bbc64a91df5136ba83
Kim Jeong-won <threedalpeng@gmail.com>
2020-08-08 12:38:19 +0900

    Create UCPC 2020 preliminary round

---
+++ Competition/UCPC/2020Pre/A.cpp
@@ -0,0 +1,28 @@
+// 수학은 비대면강의입니다 / 19532
+#include <iostream>
+using namespace std;
+
+int a, b, c, d, e, f, x, y;
+
+int main()
+{
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin >> a >> b >> c >> d >> e >> f;
+    int m = b * d - a * e, n = c * d - a * f;
+    if (m == 0)
+        if (c == 0)
+            y = 0;
+        else
+            y = c / b;
+    else
+        y = n / m;
+    if (a == 0)
+        if (d == 0)
+            x = 0;
+        else
+            x = (f - e * y) / d;
+    else
+        x = (c - b * y) / a;
+    cout << x << ' ' << y << '\n';
+}
---
+++ Competition/UCPC/2020Pre/H.cpp
@@ -0,0 +1,26 @@
+// 사과나무 / 19539
+#include <iostream>
+#include <cmath>
+using namespace std;
+using ll = long long;
+
+int n, v;
+int main()
+{
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    cin >> n;
+    ll n1 = 0, n2 = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> v;
+        n1 += (v % 2);
+        n2 += (v / 2);
+    }
+    ll k = n2 - n1;
+    if (k >= 0 && k % 3 == 0)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
